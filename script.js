76e171219992a1392ef825e4851fb6589805df6a
Dalmiro Pintos <dalmiropintos@gmail.com>
2024-04-05 15:58:57 -0300

    Correcting js links. Test.

--- script.js
+++ script.js
@@ -7,73 +7,63 @@ const emailTab = document.getElementById('email-tab');
 const browserTab = document.getElementById('browser-card');
 
 
-configButton.addEventListener('click', function() {
+configButton.addEventListener('click', function () {
     if (configTab.style.display === 'none') {
         configTab.style.display = 'block'; // Show the tab
         // tab.classList.toggle('clicked');
         configTab.style.animation = 'party 2000ms';
     } else {
-        setTimeout(function() {
+        setTimeout(function () {
             configTab.style.display = 'none'; // Hide the tab
         }, 1500);
         configTab.style.animation = 'close-tab 2000ms';
     }
 });
-emailButton.addEventListener('click', function() {
+emailButton.addEventListener('click', function () {
     if (emailTab.style.display === 'none') {
         emailTab.style.display = 'block';
         emailTab.style.animation = 'party 2000ms';
     } else {
-        setTimeout(function() {
+        setTimeout(function () {
             emailTab.style.display = 'none';
         }, 1500);
         emailTab.style.animation = 'close-tab 2000ms';
     }
 });
 
-blogButton.addEventListener('click', function() {
+blogButton.addEventListener('click', function () {
     if (browserTab.style.display === 'none') {
         browserTab.style.display = 'block';
         browserTab.style.animation = 'party 2000ms';
+        setTimeout(() => {
+            browserTab.focus();
+        }, 2050);
     } else {
-        setTimeout(function() {
+        setTimeout(function () {
             browserTab.style.display = 'none';
         }, 1500);
         browserTab.style.animation = 'close-tab 2000ms';
     }
 });
 
-// button.addEventListener('click', function() {
-//     tabContent.classList.add('show-animation');
-//     // After a short delay, remove the animation class
-//     setTimeout(function() {
-//       tabContent.classList.remove('show-animation');
-//     }, 3500); // Adjust delay based on animation duration
-//   });
+browserTab.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        setTimeout(function () {
+            browserTab.style.display = 'none';
+        }, 1500);
+        browserTab.style.animation = 'close-tab 2000ms';
+        blogButton.focus();
+    }
+
+})
+
+browserTab.setAttribute('tabindex', '-1');
 
 
-  
-// button.addEventListener('click', function() {
-//   tabContent.classList.toggle('show');
-// });
-
-
-
-// fetch('test.md')
-//   .then(response => response.text())
-//   .then(data => browserTab.innerHTML = data)
-//   .catch(err => console.error(err));
 fetch('./entries/test.md')
-  .then(response => response.text())
-  .then(markdown => marked.parse(markdown))
-  .then(html => {
-    browserTab.innerHTML = html;
-  })
-  .catch(err => console.error('Error fetching markdown file:', err));
-
-
-
-
-
-
-
+    .then(response => response.text())
+    .then(markdown => marked.parse(markdown))
+    .then(html => {
+        browserTab.innerHTML = html;
+    })
+    .catch(err => console.error('Error fetching markdown file:', err));
